30da2cadff3642d48ec542487a6f05aa5a8eacd7
Danko Milutinovic <dankomilutinovic@gmail.com>
2018-05-23 23:04:47 +0200

    adding markup and logic to the App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { INCREMENT, DECREMENT, RESET } from '../counter';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+    <div class="counter">
+      <div class="count">Current Count: {{ count$ | async }}</div>
+      <button (click)="increment()">Increment</button>
+      <button (click)="decrement()">Decrement</button>
+      <button class="btn-reset" (click)="reset()">Reset Counter</button>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  count$: Observable<number>;
+
+  constructor(private store: Store<AppState>) {
+    this.count$ = store.pipe(select('count'));
+  }
+
+  increment() {
+    this.store.dispatch({ type: INCREMENT });
+  }
+
+  decrement() {
+    this.store.dispatch({ type: DECREMENT });
+  }
+
+  reset() {
+    this.store.dispatch({ type: RESET });
+  }
 }
